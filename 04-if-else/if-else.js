b9aea6adfe32c3dc9bdb916ee5b834dd4455724b
aarahada14 <aarahada933@gmail.com>
2025-02-10 17:04:39 +0530

    bill

---
+++ 04-if-else/if-else.js
@@ -0,0 +1,18 @@
+let a=10
+let b=20
+let c=30
+
+if(a==b && b==c){
+
+    console.log("all are eqaul")
+}
+else if(a>=b && b>=c){
+
+    console.log("a is gaterthen")
+}
+else if(a<b && c<b){
+    console.log("b is gaterthen")
+}
+else{
+    console.log("c is gaterthen")
+}
