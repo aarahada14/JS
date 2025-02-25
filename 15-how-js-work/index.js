9bdd733cd393133bfd11eef479b58a19aa213eef
aarahada14 <aarahada933@gmail.com>
2025-02-25 15:32:01 +0530

    day-15

---
+++ 15-how-js-work/index.js
@@ -0,0 +1,28 @@
+let val1 = 20;
+let val2 = 30;
+
+
+
+function fun(num1, num2){
+    let result = num1 + num2  
+    fun2();
+    return result;
+}
+
+function fun2(){
+    console.log("this isfun2 ")
+}
+
+
+
+function square(n){
+    let result = n*n;
+    return result;
+}
+
+
+let result1 = fun(val1, val2);
+let result2 = fun(50, 60);
+
+let result3 = square(5)
+
