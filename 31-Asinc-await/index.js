0f8117b2105345e6cf87837ed80c5cf52f604d47
aarahada14 <aarahada933@gmail.com>
2025-04-10 15:58:10 +0530

    day-32

---
+++ 31-Asinc-await/index.js
@@ -0,0 +1,40 @@
+let url = 'https://dog.ceo/api/breed/hound/images'
+
+
+//-------------------------------------------------promisses-------------------------------------------------
+
+function callapi(){
+    fetch (url)
+    .then((res) => res.json())
+    .then((res) => {
+        console.log(res)
+        showData(res)
+    })
+    .catch(()=>{
+             console.log("something went wrong")
+    })
+}
+
+//-------------------------------------------------ASYNC AWAIT-------------------------------------------------
+
+getData()
+
+async function getData(){
+
+    try{
+       let res = await fetch(url)
+        let data = await res.json()
+
+        console.log(data)  
+    }
+    catch(err){
+        console.log(err)
+    }
+
+
+}
+
+
+function showData(data){
+
+}
